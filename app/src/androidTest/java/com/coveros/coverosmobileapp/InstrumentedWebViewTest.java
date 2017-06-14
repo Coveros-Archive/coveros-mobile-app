0066fb1bda8bc081f471537c26722e0cd792c22b
EthanTPainter <ethan.painter@coveros.com>
2017-06-14 15:54:17 -0400

    Removed tests for NoErrorWebViewClient Class

--- app/src/androidTest/java/com/coveros/coverosmobileapp/InstrumentedWebViewTest.java
+++ app/src/androidTest/java/com/coveros/coverosmobileapp/InstrumentedWebViewTest.java
@@ -18,68 +18,6 @@ public class InstrumentedWebViewTest{
     public ActivityTestRule<MainActivity> mMainActivity = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     @Test
-    public void check_ErrorTesting_1(){
-        Activity act = mMainActivity.getActivity();
-        WebView web = (WebView) act.findViewById(R.id.activity_main_webview);
-        mMainActivity.getActivity().setBrowser(web);
-        WebViewClient nec = new WebViewClient();
-        web.setWebViewClient(nec);
-
-        int errorCode = -8;
-        String problem = "This thing doesn't work";
-        String failedUrl = "https://This.Is.My.Link.com/";
-
-        nec.onReceivedError(web, errorCode, problem, failedUrl);
-        assertEquals(-8, errorCode);
-    }
-
-    @Test
-    public void check_ErrorTesting_2(){
-        Activity act = mMainActivity.getActivity();
-        WebView web = (WebView) act.findViewById(R.id.activity_main_webview);
-        mMainActivity.getActivity().setBrowser(web);
-        WebViewClient nec = new WebViewClient();
-        web.setWebViewClient(nec);
-
-        int errorCode = -6;
-        String problem = "This thing doesn't work";
-        String failedUrl = "https://This.Is.My.Link.com/";
-
-        nec.onReceivedError(web, errorCode, problem, failedUrl);
-        assertEquals(-6, errorCode);
-
-    }
-
-    @Test
-    public void check_ErrorTesting_3(){
-        Activity act = mMainActivity.getActivity();
-        WebView web = (WebView) act.findViewById(R.id.activity_main_webview);
-        mMainActivity.getActivity().setBrowser(web);
-        WebViewClient nec = new WebViewClient();
-        web.setWebViewClient(nec);
-
-        int errorCode = 106;
-        String problem = "This thing doesn't work";
-        String failedUrl = "https://This.Is.My.Link.com/";
-
-        nec.onReceivedError(web, errorCode, problem, failedUrl);
-        assertEquals(106, errorCode);
-
-    }
-
-    @Test
-    public void check_BrowserOrWebViewUsed_1() throws Exception {
-        //Browser used
-        Activity act = mMainActivity.getActivity();
-    }
-
-    @Test
-    public void check_BrowserOrWebViewUsed_2() throws Exception {
-        //WebView Client used
-
-    }
-
-    @Test
     public void check_isOnline() throws Exception {
 
     }
