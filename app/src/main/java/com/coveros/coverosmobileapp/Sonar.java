d85cd50bb20cb257f35d9a202f3e50b2e8efdc9c
sadie.rynestad <sadie.rynestad@coveros.com>
2017-06-14 11:40:11 -0400

    Sonarqube changes to run tests

---
+++ app/src/main/java/com/coveros/coverosmobileapp/Sonar.java
@@ -0,0 +1,16 @@
+package com.coveros.coverosmobileapp;
+
+/**
+ * Created by SRynestad on 6/13/2017.
+ */
+
+public class Sonar {
+    public static boolean num(int a, int b){
+                if(a > b || a == b){
+                        return true;
+                    }
+                else{
+                        return false;
+        }
+            }
+}
