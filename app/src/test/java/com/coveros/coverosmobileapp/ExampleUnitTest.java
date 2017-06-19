f73d3f7bb2c4985c04b7ee0259e242df91f21759
sadie.rynestad <sadie.rynestad@coveros.com>
2017-06-19 08:42:41 -0400

    Changes to code to see if Sonar works on Jenkins trial 2

--- app/src/test/java/com/coveros/coverosmobileapp/ExampleUnitTest.java
+++ app/src/test/java/com/coveros/coverosmobileapp/ExampleUnitTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void addition_isValid() throws Exception {
+        assertEquals(6, 3 + 3);
     }
 }
