c629e75642ed494a176d25af515e3acec8b9ed54
sadie.rynestad <sadie.rynestad@coveros.com>
2017-06-19 09:41:35 -0400

    Added SonarInstrumentedTest class

---
+++ app/src/androidTest/java/com/coveros/coverosmobileapp/SonarInstrumentedTest.java
@@ -0,0 +1,25 @@
+package com.coveros.coverosmobileapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by SRynestad on 6/12/2017.
+ */
+
+public class SonarInstrumentedTest {
+    @Test
+    public void aIsLessThanB(){
+        assertFalse(Sonar.num(6,7));
+    }
+    @Test
+    public void aIsGreaterThanB(){
+        assertTrue(Sonar.num(7,6));
+    }
+    @Test
+    public void aIsEqualtoB(){
+        assertTrue(Sonar.num(6,6));
+    }
+}
