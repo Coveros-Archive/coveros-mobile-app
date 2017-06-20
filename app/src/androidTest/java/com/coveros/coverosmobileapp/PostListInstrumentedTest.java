a085cc1b144485f969da177fc7a67f7fa4cfa1af
yeeunmariakim <yeeunmariakim@gmail.com>
2017-06-20 12:52:25 -0400

    removing iswordpressavailable

--- app/src/androidTest/java/com/coveros/coverosmobileapp/PostListInstrumentedTest.java
+++ app/src/androidTest/java/com/coveros/coverosmobileapp/PostListInstrumentedTest.java
@@ -2,17 +2,11 @@ package com.coveros.coverosmobileapp;
 
 import android.app.Activity;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Looper;
 
-import android.support.test.espresso.core.deps.guava.util.concurrent.Monitor;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import android.util.ArrayMap;
 import android.widget.ListView;
 
 import com.android.volley.NetworkResponse;
@@ -24,17 +18,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
-import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
+
+
 import static junit.framework.Assert.assertTrue;
-import static org.junit.Assume.assumeTrue;
 
 
 /**
@@ -85,66 +73,6 @@ public class PostListInstrumentedTest {
 
     }
 
-//    @Test
-//    public void retrievePosts_withoutWordpressConnection() throws InterruptedException, IOException {
-//        System.out.println("IS WORDPRESS AVAILABLE?" + isWordpressReachable(mPostListRule.getActivity()));
-//        assumeTrue(!isWordpressReachable(mPostListRule.getActivity()));
-//        Thread retrievalTest = new Thread() {
-//            public void run() {
-//                mPostListRule.getActivity().retrievePosts(new PostList.PostListCallback() {
-//                    @Override
-//                    public void onSuccess(List newItem) {
-//
-//                    }
-//                });
-//            }
-//        };
-//        retrievalTest.start();
-//        assertTrue("errorMessage should be displayed.", mPostListRule.getActivity().getErrorMessage().isShowing());
-//    }
-//
-//    @Test
-//    public void retrievePosts_withWordpressConnection() throws InterruptedException, IOException {
-//        System.out.println("IS WORDPRESS AVAILABLE?" + isWordpressReachable(mPostListRule.getActivity()));
-//        assumeTrue(isWordpressReachable(mPostListRule.getActivity()));
-//        final int beforeRetrieveLength = mPostListRule.getActivity().getPosts().size();
-//        Thread retrievalTest = new Thread() {
-//            public void run() {
-//                mPostListRule.getActivity().retrievePosts(new PostList.PostListCallback() {
-//                    @Override
-//                    public void onSuccess(List newItem) {
-//                        int afterRetrieveLength = mPostListRule.getActivity().getPosts().size();
-//                        assertEquals(afterRetrieveLength, beforeRetrieveLength + mPostListRule.getActivity().getPostsPerPage());
-//                    }
-//                });
-//            }
-//        };
-//        retrievalTest.start();
-//    }
-//
-//    public boolean isWordpressReachable(Context context) {
-//        ConnectivityManager connMan = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-//        NetworkInfo netInfo = connMan.getActiveNetworkInfo();
-//        if (netInfo != null && netInfo.isConnected()) {
-//            try {
-//                URL urlServer = new URL("wordpress.org");
-//                HttpURLConnection urlConn = (HttpURLConnection) urlServer.openConnection();
-//                urlConn.setConnectTimeout(3000); // 3 second timeout
-//                urlConn.connect();
-//                if (urlConn.getResponseCode() == 200) {
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            } catch (MalformedURLException e1) {
-//                return false;
-//            } catch (IOException e) {
-//                return false;
-//            }
-//        }
-//        return false;
-//    }
-
     public void clickItemInPostListView(PostList p, int position) {
         PostList posts = p;
         ListView postListView = posts.getPostListView();
@@ -152,16 +80,6 @@ public class PostListInstrumentedTest {
         postListView.setItemChecked(position, true);
     }
 
-    public boolean isRunning(String activity, Context context) {
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.AppTask> tasks = activityManager.getAppTasks();
-
-        for (ActivityManager.AppTask task : tasks) {
-            if (task.toString().equalsIgnoreCase(activity)) {
-                return true;
-            }
-        }
-        return false;
     }
 
 
