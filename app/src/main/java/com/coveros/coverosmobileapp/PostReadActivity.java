f5aeb7c08064290a76127a97a1f2d23fb8f8d491
Ryan Kenney <rkenney525@gmail.com>
2017-06-21 15:26:48 -0400

    Address sonar issues in PostReadActivity

--- app/src/main/java/com/coveros/coverosmobileapp/PostReadActivity.java
+++ app/src/main/java/com/coveros/coverosmobileapp/PostReadActivity.java
@@ -1,59 +1,40 @@
 package com.coveros.coverosmobileapp;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
-
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 
 /**
- * @author Maria Kim
  * Creates and displays a single blog post when it is selected from the list of blog post_list.
+ * @author Maria Kim
  */
 public class PostReadActivity extends AppCompatActivity {
 
-    TextView heading, subheading;
-    WebView content;
-    final int HEADING = 0;
-    final int SUBHEADING = 1;
-    final int CONTENT = 2;
-    final int POSITION = 3;
+    static final int HEADING_KEY = 0;
+    static final int SUBHEADING_KEY = 1;
+    static final int CONTENT_KEY = 2;
 
-    String position; // for instrumented test
     /**
      * Grabs post data from Intent and displays it.
-     * @param savedInstanceState
+     * @param savedInstanceState The saved instance state
      */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post);
 
-        ArrayList<String> post = getIntent().getStringArrayListExtra("postData");
+        List<String> post = getIntent().getStringArrayListExtra("postData");
 
-        String position = post.get(POSITION);
+        TextView heading = (TextView) findViewById(R.id.heading);
+        TextView subheading = (TextView) findViewById(R.id.subheading);
+        WebView content = (WebView) findViewById(R.id.content);
 
-        heading = (TextView) findViewById(R.id.heading);
-        subheading = (TextView) findViewById(R.id.subheading);
-        content = (WebView) findViewById(R.id.content);
-
-        heading.setText(post.get(HEADING));
-        subheading.setText(post.get(SUBHEADING));
-        content.loadData(post.get(CONTENT), "text/html; charset=utf-8", "UTF-8");
-
-
+        heading.setText(post.get(HEADING_KEY));
+        subheading.setText(post.get(SUBHEADING_KEY));
+        content.loadData(post.get(CONTENT_KEY), "text/html; charset=utf-8", "UTF-8");
     }
-
-    /**
-     * For instrumented test to check that correct post is being displayed.
-     */
-    public String getPosition() { return position; }
-
 }
