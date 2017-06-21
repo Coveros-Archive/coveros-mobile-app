16c7720c15f53f1ca86fea2d93481696e329739b
Ryan Kenney <rkenney525@gmail.com>
2017-06-21 15:16:18 -0400

    Addressing  the sonar violations from PostListAdapter

--- app/src/main/java/com/coveros/coverosmobileapp/PostListAdapter.java
+++ app/src/main/java/com/coveros/coverosmobileapp/PostListAdapter.java
@@ -2,6 +2,8 @@ package com.coveros.coverosmobileapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,27 +16,28 @@ import java.util.List;
  * Custom adapter to handle multi-line items in the postListView.
  * @author Maria Kim
  */
-public class PostListAdapter extends ArrayAdapter<Post> {
+class PostListAdapter extends ArrayAdapter<Post> {
 
-    static Context context;
-    static int layoutResourceId;
-    List<Post> data = null;
+    private final Context context;
+    private final int layoutResourceId;
+    private final List<Post> data;
 
-    public PostListAdapter(Context context, int layoutResourceId, List<Post> data) {
+    PostListAdapter(Context context, int layoutResourceId, List<Post> data) {
         super(context, layoutResourceId, data);
         this.layoutResourceId = layoutResourceId;
         this.context = context;
         this.data = data;
     }
 
+    @Override
     public long getItemId(int position) {
         return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public @NonNull View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View row = convertView;
-        PostViewHolder holder = null;
+        PostViewHolder holder;
 
         if (row == null) {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
@@ -55,7 +58,7 @@ public class PostListAdapter extends ArrayAdapter<Post> {
 
     }
 
-    static class PostViewHolder {
+    private static class PostViewHolder {
         TextView heading;
         TextView subheading;
     }
