3a25332a34eb53c29fd51327f57daf9b2f13a053
Ryan Kenney <rkenney525@gmail.com>
2017-06-21 14:34:50 -0400

    Remove sonar violations from the main activity plus refactored method to be more easily tested

--- app/src/main/java/com/coveros/coverosmobileapp/MainActivity.java
+++ app/src/main/java/com/coveros/coverosmobileapp/MainActivity.java
@@ -3,21 +3,25 @@ package com.coveros.coverosmobileapp;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.webkit.*;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //MainActivity
     private String webName;
     private WebView browser;
-    //Alerts
-    private AlertDialog dialog;
+
+    //Create Strings for Title, messsage, and buttons
+    private static final String ALERT_TTLE = "Alert";
+    private static final String ALERT_MESSAGE = "Sorry, we cannot currently retrieve the requested information.";
+    private static final String ALLERT_BUTTON_1 = "Exit App";
+    private static final String ALLERT_BUTTON_2 = "Reload App";
+    private static final String ALLERT_BUTTON_3 = "OK";
 
     public MainActivity() {
         webName = "https://www.coveros.com";
@@ -31,21 +35,10 @@ public class MainActivity extends AppCompatActivity {
         webName = website;
     }
 
-    public WebView getWebViewBrowser() {
-        return browser;
-    }
-
     public void setWebViewBrowser(WebView br) {
         browser = br;
     }
 
-    public AlertDialog getDialog() {
-        return dialog;
-    }
-
-    /*
-     * On Creation/Declaration of App/Activity
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,11 +50,6 @@ public class MainActivity extends AppCompatActivity {
         //Links open in WebView with Coveros regex check
         browser.setWebViewClient(new CustomWebViewClient() {
             @Override
-            public void onPageStarted(WebView view, String url, Bitmap fav) {
-                super.onPageStarted(view, url, fav);
-            }
-
-            @Override
             public void onPageFinished(WebView view, String url) {
                 setWebName(url);
                 super.onPageFinished(view, url);
@@ -72,9 +60,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /*
-     * On App StartUp
-     */
     @Override
     protected void onStart() {
         //Phone is online & Connected to a server
@@ -85,18 +70,13 @@ public class MainActivity extends AppCompatActivity {
             //Can be changed by either using setWebName or changing value in constructor
             browser.loadUrl(webName);
         } else {
-            try {
-                //AlertDialog
-                alertView();
-            } catch (Exception e) {
-                //Save logs if necessary
-                Log.d("Error: ", "Show Dialog: " + e.getMessage());
-            }
+            AlertDialog dialog = generateAlertView();
+            dialog.show();
         }
         super.onStart();
     }
 
-    /*
+    /**
      * Back button is pressed in the app. Default implementation
      */
     @Override
@@ -108,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /*
+    /**
      * Checks if the user is connected to the Internet
      */
     public boolean isOnline() {
@@ -117,15 +97,11 @@ public class MainActivity extends AppCompatActivity {
                 getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = conMgr.getActiveNetworkInfo();
 
-        //No internet connection
-        if (netInfo == null || !netInfo.isConnected() || !netInfo.isAvailable()) {
-            return false;
-        }
-        //Otherwise, must be connected
-        return true;
+        // NetInfo must be connected and available
+        return netInfo != null && netInfo.isConnected() && netInfo.isAvailable();
     }
 
-    /*
+    /**
      * Provides AlertDialog box if an error occurs in internet connectivity
      * Three options:   Reload App (Reload's Activity, NOT THE ENTIRE APP)
      *                  Exit App (Quits the Activity and closes the app)
@@ -134,17 +110,13 @@ public class MainActivity extends AppCompatActivity {
      * OK option provided to immediately close the dialog box if the user's wifi loads
      *      the Coveros website in the background
      */
-    private void alertView() {
-        //Create Strings for Title, messsage, and buttons
-        String title = "Alert";
-        String message = "Sorry, we cannot currently retrieve the requested information.";
-        String button1 = "Exit App";
-        String button2 = "Reload App";
-        String button3 = "OK";
-
-        //Init Alert Dialog menu & Cancel only if pressed on button
-        dialog = new AlertDialog.Builder(MainActivity.this)
-                .setNeutralButton(button2, new DialogInterface.OnClickListener() {
+    private AlertDialog generateAlertView() {
+        // Init Alert Dialog menu & Cancel only if pressed on button
+        AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle(ALERT_TTLE)
+                .setMessage(ALERT_MESSAGE)
+                .setCancelable(false)
+                .setNeutralButton(ALLERT_BUTTON_2, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(getApplicationContext(), "Loading App", Toast.LENGTH_SHORT).show();
@@ -152,40 +124,22 @@ public class MainActivity extends AppCompatActivity {
                         recreate();
                     }
                 })
-                .setNegativeButton(button3, new DialogInterface.OnClickListener() {
+                .setNegativeButton(ALLERT_BUTTON_3, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
                 })
-                .setPositiveButton(button1, new DialogInterface.OnClickListener() {
+                .setPositiveButton(ALLERT_BUTTON_1, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(getApplicationContext(), "Thank You", Toast.LENGTH_SHORT).show();
                         dialog.dismiss();
                         finish();
                     }
-                }).create();
-        dialog.setCancelable(false);
+                })
+                .create();
         dialog.setCanceledOnTouchOutside(false);
-        //Setters (title, default message, button 1 -> Exit, button2 -> Reload)
-        dialog.setTitle(title);
-        dialog.setMessage(message);
-        //Show dialog and make text changes (font color, size, etc.)
-        dialog.show();
-    }
-
-    /*
-     * Purpose: Saving Scrolled position on Web Pages and coming back to for later viewing(onPause)
-     * Calculates Scrolling Percentage to return to precisely same location
-     * on webpage. Previously onResume after onPause results in user being
-     * pushed to the top of a webpage, losing track of where/what they were
-     * looking at
-     */
-    private float calculateProgression(WebView content) {
-        float contentHeight = content.getContentHeight();
-        float currentScrollPosition = content.getScrollY();
-        float percentWebview = currentScrollPosition / contentHeight;
-        return percentWebview;
+        return dialog;
     }
 }
