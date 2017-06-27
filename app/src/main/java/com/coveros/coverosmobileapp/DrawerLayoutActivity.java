04153b4290ec06a30c8210363c3ab6524f24c0e7
sadie.rynestad <sadie.rynestad@coveros.com>
2017-06-27 14:35:10 -0400

    Menu Drawer with onClick to Activities

--- app/src/main/java/com/coveros/coverosmobileapp/DrawerLayoutActivity.java
+++ app/src/main/java/com/coveros/coverosmobileapp/DrawerLayoutActivity.java
@@ -1,17 +1,22 @@
 package com.coveros.coverosmobileapp;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.coveros.coverosmobileapp.post.PostListActivity;
+import com.coveros.coverosmobileapp.website.MainActivity;
+
 
 /**
  * Created by SRynestad on 6/26/2017.
  */
 
 public class DrawerLayoutActivity extends ListActivity {
-    private String[] menuTitles = new String[]{"Menu","Blog","Team"};
+    private String[] menuTitles = new String[]{"Menu","Blog","Bookmarks"};
     private ListView menuDrawerList;
 
     @Override
@@ -25,8 +30,21 @@ public class DrawerLayoutActivity extends ListActivity {
         menuDrawerList.setAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1 , menuTitles));
 
-        // Set the list's click listener
-       //menuDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        menuDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position) {
+                    case 0:
+                        Intent a = new Intent(DrawerLayoutActivity.this, MainActivity.class);
+                        startActivity(a);
+                        break;
 
+                    case 1:
+                        Intent b = new Intent(DrawerLayoutActivity.this, PostListActivity.class);
+                        startActivity(b);
+                        break;
+                }
+            }
+        });
     }
 }
