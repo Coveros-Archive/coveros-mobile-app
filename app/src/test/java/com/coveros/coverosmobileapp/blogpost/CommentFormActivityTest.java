45dabde7396d112fce789745f06cb19c22303680
yeeunmariakim <yeeunmariakim@gmail.com>
2017-07-05 14:19:02 -0400

    changing variables from global to local in CommentFormActivityTest

--- app/src/test/java/com/coveros/coverosmobileapp/blogpost/CommentFormActivityTest.java
+++ app/src/test/java/com/coveros/coverosmobileapp/blogpost/CommentFormActivityTest.java
@@ -12,19 +12,19 @@ import static junit.framework.Assert.assertEquals;
 
 public class CommentFormActivityTest {
 
-    private static final String EXPECTED_AUTHOR_STRING = "name";
-    private static final String EXPECTED_EMAIL_STRING = "email";
-    private static final String EXPECTED_MESSAGE_STRING = "message";
-
     @Test
     public void checkFieldIsEmpty_withThreeEmptyStrings() {
+        String expectedAuthorName = "name";
+        String expectedEmailString = "email";
+        String expectedMessageString = "message";
+
         List<String> actualEmptyFields = CommentFormActivity.checkFieldIsEmpty("", "", "");
         String actualAuthorString = actualEmptyFields.get(0);
         String actualEmailString = actualEmptyFields.get(1);
         String actualMessageString = actualEmptyFields.get(2);
-        assertEquals("\"Author\" strings should match", EXPECTED_AUTHOR_STRING, actualAuthorString);
-        assertEquals("\"Email\" strings should match", EXPECTED_EMAIL_STRING, actualEmailString);
-        assertEquals("\"Message\" strings should match", EXPECTED_MESSAGE_STRING, actualMessageString);
+        assertEquals("\"Author\" strings should match", expectedAuthorName, actualAuthorString);
+        assertEquals("\"Email\" strings should match", expectedEmailString, actualEmailString);
+        assertEquals("\"Message\" strings should match", expectedMessageString, actualMessageString);
 
     }
 
