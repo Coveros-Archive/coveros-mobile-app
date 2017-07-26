0dbd15c2e41ed26abb6873bf059ae146bcdb74fd
yeeunmariakim <yeeunmariakim@gmail.com>
2017-07-26 09:50:40 -0400

    adding java folder

---
+++ app/src/appiumTest/java/com/coveros/coverosmobileapp/BlogPostReadActivityAppiumTest.java
@@ -0,0 +1,44 @@
+package com.coveros.coverosmobileapp;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BlogPostReadActivityAppiumTest {
+    public AndroidDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "7.1.1");
+        capabilities.setCapability("appPackage", "com.coveros.coverosmobileapp");
+        capabilities.setCapability("appActivity", ".blogpost.BlogPostReadActivity");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void viewComments() {
+        driver.findElementById("com.coveros.coverosmobileapp:id/view_comments").click();
+
+        String expectedCommentLabel = "Comments";
+        String actualCommentLabel = driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.TextView").getText();
+
+        assertThat(actualCommentLabel, equalTo(expectedCommentLabel));
+    }
+
+
+}
+
