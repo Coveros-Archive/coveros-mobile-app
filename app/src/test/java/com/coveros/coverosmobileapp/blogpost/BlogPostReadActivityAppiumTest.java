44cb9f4486895e37764fe4d90350ebe88c071d68
yeeunmariakim <yeeunmariakim@gmail.com>
2017-07-26 10:54:42 -0400

    changing test bc of the changed default value

--- app/src/test/java/com/coveros/coverosmobileapp/blogpost/BlogPostReadActivityAppiumTest.java
+++ app/src/test/java/com/coveros/coverosmobileapp/blogpost/BlogPostReadActivityAppiumTest.java
@@ -23,7 +23,7 @@ public class BlogPostReadActivityAppiumTest {
         capabilities.setCapability("deviceName", "Pixel");
         capabilities.setCapability("platformVersion", "7.1.1");
         capabilities.setCapability("appPackage", "com.coveros.coverosmobileapp");
-        capabilities.setCapability("appActivity", ".blogpost.BlogPostReadActivity");
+        capabilities.setCapability("appActivity", ".blogpost.BlogPostsListActivity");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -31,6 +31,8 @@ public class BlogPostReadActivityAppiumTest {
 
     @Test
     public void viewComments() {
+        // click on the first blog post in the list
+        driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.RelativeLayout[1]").click();
         driver.findElementById("com.coveros.coverosmobileapp:id/view_comments").click();
 
         String expectedCommentLabel = "Comments";
