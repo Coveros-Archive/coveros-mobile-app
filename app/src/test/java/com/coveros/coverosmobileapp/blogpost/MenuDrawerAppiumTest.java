6a0d3cfa5052473e8b911d485d61b402755d337c
sadie.rynestad <sadie.rynestad@coveros.com>
2017-07-27 16:37:26 -0400

    Added menu drawer appium test

---
+++ app/src/test/java/com/coveros/coverosmobileapp/blogpost/MenuDrawerAppiumTest.java
@@ -0,0 +1,62 @@
+package com.coveros.coverosmobileapp.blogpost;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+
+/**
+ * Created by SRynestad on 7/27/2017.
+ */
+
+public class MenuDrawerAppiumTest {
+    private AndroidDriver driver;
+    public void swipe()
+    {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, Double> swipeObject = new HashMap<String, Double>();
+        swipeObject.put("startX", 0.95);
+        swipeObject.put("startY", 0.5);
+        swipeObject.put("endX", 0.05);
+        swipeObject.put("endY", 0.5);
+        swipeObject.put("duration", 1.8);
+        js.executeScript("mobile: swipe", swipeObject);
+    }
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "7.1.1");
+        capabilities.setCapability("appPackage", "com.coveros.coverosmobileapp");
+        capabilities.setCapability("appActivity", ".website.MainActivity");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+    @Test
+    public void menuDrawer_slidesOpenInWebview(){
+        driver.context("WEBVIEW");
+        swipe();
+        //make an assertion
+    }
+    @Test
+    public void menuDrawer_slidesOpenInListOfBlogPosts(){
+        driver.context("WEBVIEW");
+        swipe();
+        driver.findElementByName("Blog");
+        swipe();
+        //make an assertion
+    }
+}
