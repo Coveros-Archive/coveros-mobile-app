40288991d58bd59c499814866de08cd04e532f98
yeeunmariakim <yeeunmariakim@gmail.com>
2017-07-27 16:30:34 -0400

    remove retrieveAuthors and retrieveBlogPosts methods from BlogPostsListActivity

--- app/src/main/java/com/coveros/coverosmobileapp/blogpost/BlogPostsListActivity.java
+++ app/src/main/java/com/coveros/coverosmobileapp/blogpost/BlogPostsListActivity.java
@@ -12,19 +12,17 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.coveros.coverosmobileapp.R;
 import com.coveros.coverosmobileapp.dialog.AlertDialogFactory;
 import com.coveros.coverosmobileapp.errorlistener.NetworkErrorListener;
+import com.coveros.coverosmobileapp.oauth.RestRequest;
 import com.coveros.coverosmobileapp.website.MainActivity;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +37,11 @@ public class BlogPostsListActivity extends BlogListActivity {
 
     private static final int POSTS_PER_PAGE = 10;
     private static final int NUM_OF_AUTHORS = 100;  // number of users that will be returned by the REST call... so if someday Coveros has over 100 employees, this needs to be changed
+    private static final int DRAWER_OPEN_BLOG_MENU_POSITION = 12;
+
     private static final String AUTHORS_URL = "https://www3.dev.secureci.com/wp-json/wp/v2/users?orderby=id&per_page=" + NUM_OF_AUTHORS;
+    private static final String POSTS_URL = "https://www3.dev.secureci.com/wp-json/wp/v2/posts?per_page=" + POSTS_PER_PAGE + "&order=desc&orderby=date&fields=id,title,date,author&offset=%d";
+    private static final String DEVSITE_URL = "https://www3.dev.secureci.com/";
 
     private List<BlogPost> blogPosts = new ArrayList<>();
     private SparseArray<String> authors = new SparseArray<>();  // to aggregate the ids and names of the authors of displayed blog posts
@@ -49,12 +51,11 @@ public class BlogPostsListActivity extends BlogListActivity {
     private LinearLayout postList;
     private ListView blogPostsListView;
     private BlogPostsListAdapter postsAdapter;
+
     private int currentListSize;
-    static final int DRAWER_OPEN_BLOG_MENU_POSITION = 12;
     private int postsOffset = 0;
 
-    private static final String POSTS_URL = "https://www3.dev.secureci.com/wp-json/wp/v2/posts?per_page=" + POSTS_PER_PAGE + "&order=desc&orderby=date&fields=id,title,date,author&offset=%d";
-    static final String DEVSITE = "https://www3.dev.secureci.com/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,9 +88,35 @@ public class BlogPostsListActivity extends BlogListActivity {
                                }
         );
 
-        // running these requests on a separate thread for performance
-        Thread requestsThread = new RequestsThread();
-        requestsThread.start();
+        requestQueue = Volley.newRequestQueue(this);
+        RestRequest authorsRequest = new RestRequest(AUTHORS_URL, null, null, new Response.Listener<JsonObject>() {
+            @Override
+            public void onResponse(JsonObject response) {
+                for (JsonElement author : response.get("response").getAsJsonArray()) {
+                    JsonObject authorJson = (JsonObject) author;
+                    Integer id = authorJson.get("id").getAsInt();
+                    authors.put(id, authorJson.get("name").getAsString());
+                }
+                RestRequest blogPostsRequest = new RestRequest(String.format(Locale.US, POSTS_URL, postsOffset), null, null, new Response.Listener<JsonObject>() {
+                    @Override
+                    public void onResponse(JsonObject response) {
+                        JsonArray blogPostsJson = response.get("response").getAsJsonArray();
+                        List<BlogPost> newBlogPosts = new ArrayList<>();
+                        for (JsonElement blogPost : blogPostsJson) {
+                            newBlogPosts.add(new BlogPost((JsonObject) blogPost, authors));
+                        }
+                        blogPosts.addAll(newBlogPosts);
+                        postsAdapter = new BlogPostsListAdapter(BlogPostsListActivity.this, R.layout.post_list_text, blogPosts);
+                        blogPostsListView.setAdapter(postsAdapter);
+                        currentListSize = blogPostsListView.getAdapter().getCount();
+                        blogPostsListView.setOnScrollListener(new BlogPostsListOnScrollListener());
+                        postsOffset = postsOffset + POSTS_PER_PAGE;
+                    }
+                }, networkErrorListener);
+                requestQueue.add(blogPostsRequest);
+            }
+        }, networkErrorListener);
+        requestQueue.add(authorsRequest);
 
         // when a post is selected, feeds its associated data into a BlogPostReadActivity activity
         blogPostsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -110,96 +137,20 @@ public class BlogPostsListActivity extends BlogListActivity {
      * Adds blogPosts to the list view. Called when user scrolls to the bottom of the listview.
      */
     protected void addPosts() {
-        Thread addPostRequest = new Thread() {
+        RestRequest blogPostsRequest = new RestRequest(String.format(Locale.US, POSTS_URL, postsOffset), null, null, new Response.Listener<JsonObject>() {
             @Override
-            public void run() {
-                retrieveBlogPosts(new PostListCallback<BlogPost>() {
-                    @Override
-                    public void onSuccess(List<BlogPost> newPosts) {
-                        postsAdapter.addAll(newPosts);
-                        postsAdapter.notifyDataSetChanged();
-                    }
-                });
-            }
-        };
-        addPostRequest.start();
-    }
-
-    /**
-     * Custom thread to run requests for post and author data.
-     */
-    class RequestsThread extends Thread {
-        @Override
-        public void run() {
-            requestQueue = Volley.newRequestQueue(BlogPostsListActivity.this);
-            retrieveAuthors(new PostListCallback<String>() {
-                @Override
-                public void onSuccess(List<String> newAuthors) {
-                    retrieveBlogPosts(new PostListCallback<BlogPost>() {
-                        @Override
-                        public void onSuccess(List<BlogPost> newPosts) {
-                            blogPosts.addAll(newPosts);
-                            postsAdapter = new BlogPostsListAdapter(BlogPostsListActivity.this, R.layout.post_list_text, blogPosts);
-                            blogPostsListView.setAdapter(postsAdapter);
-                            currentListSize = blogPostsListView.getAdapter().getCount();
-                            blogPostsListView.setOnScrollListener(new BlogPostsListOnScrollListener());
-                        }
-
-                    });
-                }
-            });
-        }
-    }
-
-    /**
-     * Populates List of Authors.
-     *
-     * @param postListCallback A callback function to be executed after the list of authors has been retrieved
-     */
-    protected void retrieveAuthors(final PostListCallback<String> postListCallback) {
-        StringRequest authorsRequest = new StringRequest(Request.Method.GET, AUTHORS_URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                JsonArray authorsJson = new JsonParser().parse(response).getAsJsonArray();
-                for (JsonElement author : authorsJson) {
-                    JsonObject authorJson = (JsonObject) author;
-                    Integer id = authorJson.get("id").getAsInt();
-                    authors.put(id, authorJson.get("name").getAsString());
-                }
-
-                postListCallback.onSuccess(null);
-            }
-        }, networkErrorListener);
-        requestQueue.add(authorsRequest);
-    }
-
-    /**
-     * Populates List of Posts.
-     *
-     * @param postListCallback A callback function to be executed after the list of blogPosts has been retrieved
-     */
-    protected void retrieveBlogPosts(final PostListCallback<BlogPost> postListCallback) {
-        StringRequest blogPostsRequest = new StringRequest(Request.Method.GET, String.format(Locale.US, POSTS_URL, postsOffset), new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                JsonArray blogPostsJson = new JsonParser().parse(response).getAsJsonArray();
+            public void onResponse(JsonObject response) {
+                JsonArray blogPostsJson = response.get("response").getAsJsonArray();
                 List<BlogPost> newBlogPosts = new ArrayList<>();
                 for (JsonElement blogPost : blogPostsJson) {
                     newBlogPosts.add(new BlogPost((JsonObject) blogPost, authors));
                 }
-                postListCallback.onSuccess(newBlogPosts);
                 postsOffset = postsOffset + POSTS_PER_PAGE;
+                postsAdapter.addAll(newBlogPosts);
+                postsAdapter.notifyDataSetChanged();
             }
         }, networkErrorListener);
-
         requestQueue.add(blogPostsRequest);
-    }
-
-    /**
-     * Used to ensure StringRequests are completed before their data are used.
-     */
-    interface PostListCallback<T> {
-        void onSuccess(List<T> newItems);
     }
 
     /**
@@ -246,7 +197,7 @@ public class BlogPostsListActivity extends BlogListActivity {
             if (position == DRAWER_OPEN_BLOG_MENU_POSITION) {
                 menu.closeDrawer(Gravity.START);
             } else {                                     //Return to a web page in Main Activity
-                newIntent.putExtra("WEBSITE", DEVSITE + stringsToAddLast[position]);
+                newIntent.putExtra("WEBSITE", DEVSITE_URL + stringsToAddLast[position]);
                 startActivity(newIntent);
             }
         }
