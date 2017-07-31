013ea219978e1f6ac203e82cb09a569a37b73927
sadie.rynestad <sadie.rynestad@coveros.com>
2017-07-31 12:29:29 -0400

    appium testing

--- app/src/test/java/com/coveros/coverosmobileapp/blogpost/MenuDrawerAppiumTest.java
+++ app/src/test/java/com/coveros/coverosmobileapp/blogpost/MenuDrawerAppiumTest.java
@@ -1,22 +1,19 @@
 package com.coveros.coverosmobileapp.blogpost;
 
-import android.widget.ListView;
-
-import com.coveros.coverosmobileapp.R;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
+
 import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.android.AndroidDriver;
@@ -32,21 +29,30 @@ public class MenuDrawerAppiumTest {
     private AndroidDriver driver;
     public void swipe() throws InterruptedException
     {
-            //Get the size of screen.
-            Dimension size = driver.manage().window().getSize();
-            System.out.println(size);
-
-            //Find swipe start and end point from screen's with and height.
-            //Find startx point which is at right side of screen.
-            int startx = (int) (size.width * 0.70);
-            //Find endx point which is at left side of screen.
-            int endx = (int) (size.width * 0.30);
-            //Find vertical point where you wants to swipe. It is in middle of screen height.
-            int starty = size.height / 2;
-            System.out.println("startx = " + startx + " ,endx = " + endx + " , starty = " + starty);
-            //Swipe from Left to Right.
-            driver.swipe(endx, starty, startx, starty, 3000);
-            Thread.sleep(2000);
+        JavascriptExecutor js = driver;
+        HashMap<String, Double> swipeObject = new HashMap<String, Double>();
+        swipeObject.put("startX", 0.05);
+        swipeObject.put("startY", 0.5);
+        swipeObject.put("endX", 0.95);
+        swipeObject.put("endY", 0.5);
+        swipeObject.put("duration", 1.8);
+        js.executeScript("mobile: swipe", swipeObject);
+        Thread.sleep(2000);
+//            //Get the size of screen.
+//            Dimension size = driver.manage().window().getSize();
+//            System.out.println(size);
+//
+//            //Find swipe start and end point from screen's with and height.
+//            //Find startx point which is at right side of screen.
+//            int startx = (int) (size.width * 0.70);
+//            //Find endx point which is at left side of screen.
+//            int endx = (int) (size.width * 0.30);
+//            //Find vertical point where you wants to swipe. It is in middle of screen height.
+//            int starty = size.height / 2;
+//            System.out.println("startx = " + startx + " ,endx = " + endx + " , starty = " + starty);
+//            //Swipe from Left to Right.
+//            driver.swipe(endx, starty, startx, starty, 3000);
+//            Thread.sleep(2000);
     }
     @Before
     public void setUp() throws Exception {
@@ -56,6 +62,7 @@ public class MenuDrawerAppiumTest {
         capabilities.setCapability("platformVersion", "7.1.1");
         capabilities.setCapability("appPackage", "com.coveros.coverosmobileapp");
         capabilities.setCapability("appActivity", ".website.MainActivity");
+        capabilities.setJavascriptEnabled(true);
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -66,15 +73,9 @@ public class MenuDrawerAppiumTest {
     }
     @Test
     public void menuDrawer_slidesOpenInWebview() throws InterruptedException{
-        Set<String> contextNames = driver.getContextHandles();
-        System.out.println(contextNames.toString());
-     for (String contextName : contextNames) {
-           if (contextName.contains("WEBVIEW")) {
-                driver.context(contextName);
-                System.out.println(driver.getContext());
-            }
-        }
         swipe();
+        WebElement drawer = driver.findElement(By.id("left_drawer"));
+        assertThat(drawer.isDisplayed(), is(true));
 
     }
     @Test
